63bd7e0c35c3f7299217a34e6709d2cda22999a9
black silence <pat@psycho3d.de>
2016-11-19 09:41:07 +0100

    update to wayward 2.2

--- legendaryrefresh.js
+++ legendaryrefresh.js
@@ -1,31 +1,26 @@
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-var Mod = (function (_super) {
-    __extends(Mod, _super);
-    function Mod() {
-        _super.apply(this, arguments);
-    }
-    Mod.prototype.onInitialize = function (saveDataGlobal) {
-    };
-    Mod.prototype.onLoad = function (saveData) {
-    };
-    Mod.prototype.onUnload = function () {
-    };
-    Mod.prototype.onSave = function () {
-    };
-    Mod.prototype.onTurnComplete = function () {
-        var items = Item.getItemsInContainer(player.inventory);
-        for (var i = items.length - 1; i >= 0; i--) {
-            if (items[i].quality != ItemQuality.Legendary) {
-                continue;
-            }
-            if (items[i].minDur < items[i].maxDur) {
-                items[i].minDur = items[i].maxDur;
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    class Mod extends Mods.Mod {
+        onInitialize(saveDataGlobal) {
+        }
+        onLoad(saveData) {
+        }
+        onUnload() {
+        }
+        onSave() {
+        }
+        onTurnComplete() {
+            let items = Item.getItemsInContainer(player.inventory);
+            for (let i = items.length - 1; i >= 0; i--) {
+                if (items[i].quality != ItemQuality.Legendary) {
+                    continue;
+                }
+                if (items[i].minDur < items[i].maxDur) {
+                    items[i].minDur = items[i].maxDur;
+                }
             }
         }
-    };
-    return Mod;
-}(Mods.Mod));
+    }
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = Mod;
+});
